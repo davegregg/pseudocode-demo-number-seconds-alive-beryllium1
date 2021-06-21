10bfdf5dc650c42a3fdf03a07284429eb506474a
David Michael Gregg <davegregg@gmail.com>
2021-06-21 20:40:37 +0000

    Add comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Step 1: Get user input!
 let birthYearInput = prompt("What year were you born?")
 let birthMonthInput = prompt("What month were you born?")
 let birthYear = parseInt(birthYearInput)
@@ -7,6 +8,7 @@ let birthMonth = parseInt(birthMonthInput)
 // then alert the user to retry
 let inputIsInvalid = Number.isNaN(birthYear) || Number.isNaN(birthMonth)
 if (inputIsInvalid) {
+
     document.write(`
         <h3>
             "${birthYearInput}" and/or "${birthMonthInput}" is invalid input. 
@@ -14,24 +16,34 @@ if (inputIsInvalid) {
             a number (for example, "6" for June).
         </h3>
     `)
-} else {
+
+} else {  // Otherwise... We have the user-supplied information we need and we're ready to continue!
+
+    // Step 2: Get current year and month as numbers!
     let currentYear = new Date().getFullYear()
     let currentMonth = new Date().getMonth() + 1
-    
+
+    // Step 3: Calculate the number of seconds in a month/year.
     const secondsInMonth = 30 * 24 * 60 * 60
     const secondsInYear = secondsInMonth * 12
     
+    // Step 4: Calculate the number of seconds in the user's birth year.
     let secondsInBirthYear = (12 - birthMonth) * secondsInMonth
+    // Step 5: Calculate the number of seconds in the current year.
     let secondsInCurrentYear = currentMonth * secondsInMonth
+    // Step 6: Calculate the number of seconds total in all of the years in between their birth year and the current year.
     let secondsInBetween = (currentYear - birthYear - 2) * secondsInYear
     
+    // Step 7: Calculate the final result by adding the above three values together, giving us the total approximate number of seconds since birth.
     let secondsSinceBirth = secondsInBirthYear + secondsInBetween + secondsInCurrentYear
     let secondsSinceBirth_LOCALIZED = secondsSinceBirth.toLocaleString()
 
+    // Step 8: Output!
     console.log({ currentYear, currentMonth, birthYear, birthMonth, secondsSinceBirth_LOCALIZED })
     document.write(`
         <h3>
             You have been alive for approximately ${secondsSinceBirth_LOCALIZED} seconds!
         </h3>
     `)
+
 }
